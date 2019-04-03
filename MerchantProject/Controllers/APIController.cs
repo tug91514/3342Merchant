f1a89651c5e9233cfc4b9b17e57e0230afab70bd
James Zawierucha <tug91514@temple.edu>
2019-04-03 08:56:16 -0400

    Add Project

--- MerchantProject/Controllers/APIController.cs
+++ MerchantProject/Controllers/APIController.cs
@@ -2,40 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MerchantProject.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Produces("application/json")]
+    [Route("api/service/Merchants")]
+    public class APIController : Controller
     {
-        // GET api/values
+        // GET: api/API
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        // GET: api/API/5
+        [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
+        
+        // POST: api/API
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        
+        // PUT: api/API/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
