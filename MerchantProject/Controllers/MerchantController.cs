dfad2f6feeba6e7ccca4e1d207327a5250322a3c
Robert Convery <tuc47460@temple.edu>
2019-04-03 22:54:23 -0400

    Created new API. Old API Missing startup Files

---
+++ MerchantProject/Controllers/MerchantController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Utilities;
+
+namespace MerchantProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Service")]
+    public class MerchantController : Controller
+    {
+        [HttpGet] // GET api/Service/
+        [HttpGet("Departments")]   // GET api/Service/Departments
+        public DataSet GetAllDepartments()
+        {
+            //local var
+            DBConnect dbconn = new DBConnect();
+            string sqlcomm = "SELECT * FROM term_Department";
+            DataSet myDataSet = new DataSet();
+
+            try
+            {
+                //get dataset
+                myDataSet = dbconn.GetDataSet(sqlcomm);
+
+                //return DataSet
+                return myDataSet;
+            }
+            catch (SqlException sqlex)
+            {
+                return null;
+            }//end catch sql ex
+            catch (Exception ex)
+            {
+                return null;
+            }//end catch
+        }
+
+        // GET: api/API/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/API
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/API/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MerchantProject/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MerchantProject.Models
+{
+    public class Customer
+    {
+        int CustomerId { get; set; }
+        string UserName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+        string PhoneNum { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+
+        public Customer() { } //default constructor
+    }
+}
